da6780db728ad2eae12a2f3287c4fda49e941d5a
Andatax <abraham.mendez1011@gmail.com>
2023-09-14 14:27:24 -0600

    Added function to create characters for password

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
+const passwordEl = document.querySelector("#password");
+const upperChecker = document.querySelector("#uppercase");
+const generateBtn = document.querySelector("#generate");
+const lowerChecker = document.querySelector("#lowercase");
+const numChecker = document.querySelector("#num");
+const lengthVal = document.querySelector("#length");
+const specialcChecker = document.querySelector("sCharacters");
+
+const upper = upperChecker.checked;
+
+const num = numChecker.checked;
+const lower = lowerChecker.checked;
+const sym = specialcChecker.checked;
+const length = lengthVal.value;
+let settingsNum = upper + lower + sym + numChecker;
+
+// adding lowercase letters to an array
+var lowC = charactersArray(26,97);
+var upC = charactersArray(26,65);
+var symA = charactersArray(11,33);
+var numA = charactersArray(10,48);
+
+// console.log(lowC + upC + symA);
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
+function charactersArray(param1,param2) {
+  let arr = [];
+  for( let i = 0; i < param1; i++){
+  arr.push(String.fromCharCode(param2 + i));
+  }
+return arr;
+}
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -7,15 +40,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", generateSettings);
-
-function generateSettings(){
-  document.getElementById("show-buttons").style.display = 'block';  
-
-
-}
 
