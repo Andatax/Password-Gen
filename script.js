799ac7a9bba0f58b56f6a7b825ff839262cac8b3
Andatax <abraham.mendez1011@gmail.com>
2023-09-15 11:20:37 -0600

    Added functions to select letters, randomize and filter settings

--- script.js
+++ script.js
@@ -4,35 +4,53 @@ const generateBtn = document.querySelector("#generate");
 const lowerChecker = document.querySelector("#lowercase");
 const numChecker = document.querySelector("#num");
 const lengthVal = document.querySelector("#length");
-const specialcChecker = document.querySelector("sCharacters");
+const scChecker = document.querySelector("#sCharacters");
 
 const upper = upperChecker.checked;
-
 const num = numChecker.checked;
 const lower = lowerChecker.checked;
-const sym = specialcChecker.checked;
+const sym = scChecker.checked;
 const length = lengthVal.value;
-let settingsNum = upper + lower + sym + numChecker;
+
+const settings = { upper, num, lower, sym };
+const settingsC = Object.entries(settings).filter(([key, value]) => value === true);
+const FilteredSettings = {};
+const fsObj = settingsC.reduce((object, [key]) => { object[key] = true; return object; }, FilteredSettings);
+const acumm = (upper + num + lower + sym);
 
 // adding lowercase letters to an array
-var lowC = charactersArray(26,97);
-var upC = charactersArray(26,65);
-var symA = charactersArray(11,33);
-var numA = charactersArray(10,48);
-
-// console.log(lowC + upC + symA);
-// Assignment Code
-
-function charactersArray(param1,param2) {
+let lowC = charactersArray(26, 97);
+// adding uppercase letters to an array
+let upC = charactersArray(26, 65);
+// adding special characters to an array
+let symA = charactersArray(11, 33);
+// adding numbers to an array;
+let numA = charactersArray(10, 48);
+function charactersArray(alphabetLength, ASCIIcodeOffset) {
   let arr = [];
-  for( let i = 0; i < param1; i++){
-  arr.push(String.fromCharCode(param2 + i));
+  for (let i = 0; i < alphabetLength; i++) {
+    arr.push(String.fromCharCode(ASCIIcodeOffset + i));
   }
-return arr;
+  return arr;
 }
-
-
-
+// random selector from character arrays
+function randomSelector(CharacterArray) {
+  const array = CharacterArray;
+  const arrayR = Math.floor(Math.random() * array.length);
+  const indexR = array[arrayR];
+  return indexR
+}
+// how can i make a code to run the index of the array 'fsObj'
+// I need to make a variable with and empty string to that every time for cycle iterates it adds in character of the array 'fsObj' options.
+function generatePassword(param) {
+  let pass = '';
+  for (let i = 0; i < param; i + acumm) {
+    fsObj.forEach((element) => pass += randomSelector(element))
+  }
+  return pass;
+}
+const finalPassword = generatePassword(10);
+console.log(finalPassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -42,6 +60,6 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", generateSettings);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", generateSettings);
 
