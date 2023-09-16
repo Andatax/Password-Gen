9cfe0caaf1c1659364214c26a6cf830390991b18
Andatax <abraham.mendez1011@gmail.com>
2023-09-16 17:39:05 -0600

    added function to create characters arrays

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function generatePassword() {
+  // adding lowercase letters to an array
+  let lowC = charactersArray(26, 97);
+  // adding uppercase letters to an array
+  let upC = charactersArray(26, 65);
+  // adding special characters to an array
+  let symA = charactersArray(11, 33);
+  // adding numbers to an array;
+  let numA = charactersArray(10, 48);
+  let characterArray = lowC.concat(upC,symA, numA);
+  console.log(characterArray)
   return 'hello'
 }
 
-
+function charactersArray(alphabetLength, ASCIIcodeOffset) {
+  let arr = [];
+  for (let i = 0; i < alphabetLength; i++) {
+    arr.push(String.fromCharCode(ASCIIcodeOffset + i));
+  }
+  return arr;
+}
 
 
 // Assignment Code
